82ad1a673d9579009f62d32e4ebe0754026626ad
Aaron Paredes Cabrera <iron0406@outlook.com>
2022-06-24 22:42:37 -0500

    chore: init

---
+++ src/main/java/com/example/bookisbackend/BookisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bookisbackend/BookisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
